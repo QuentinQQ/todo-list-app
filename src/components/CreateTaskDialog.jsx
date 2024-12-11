379d4291a0e97007117cb884099107c944030203
QuentinQ <quentin.zt.qian@gmail.com>
2024-12-11 23:15:40 +1100

    update

--- src/components/CreateTaskDialog.jsx
+++ src/components/CreateTaskDialog.jsx
@@ -1,18 +1,15 @@
-import React from 'react';
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
 } from '@/components/ui/dialog';
 import { Button } from '@/components/ui/Button';
 import { Input } from '@/components/ui/Input';
 import { Label } from '@/components/ui/Label';
 
-const CreateTaskDialog = ({ isOpen, onClose }) => {
+const CreateTaskDialog = ({ isOpen, onClose, inputRef }) => {
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
       <DialogContent className="sm:max-w-[425px] flex max-w-md flex-col w-11/12 p=7 rounded-xl">
@@ -21,7 +18,7 @@ const CreateTaskDialog = ({ isOpen, onClose }) => {
         </DialogHeader>
         <div className="grid gap-4 py-4">
           <div className="grid grid-cols-4 items-center gap-4">
-            <Input id="taskName" placeholder="Add Title" className="col-span-3" />
+            <Input id="taskName" placeholder="Add Title" className="col-span-3" ref={inputRef}/>
           </div>
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="taskDescription" className="text-right">
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,12 +1,13 @@
 import todo_icon from '../assets/rectangle-list-solid.svg'
 import { Button } from '@/components/ui/Button';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import CreateTaskDialog from './CreateTaskDialog';
 
 const Todo = () => {
   const [isDialogOpen, setDialogOpen] = useState(false);
+  const inputRef = useRef();
 
-  const handleCreateButtonClick = () => {
+  const handleAddButtonClick = () => {
     setDialogOpen(true);
   }
 
@@ -14,14 +15,12 @@ const Todo = () => {
     setDialogOpen(false);
   }
 
+  const submitAddTask = () => {
+	
+  }
+
   return (
     <div className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
-        {/* Title */}
-        {/* <div className='flex items-center mt-7 gap-2'>
-            <img className='w-8' src={todo_icon} alt="Todo Icon" />
-            <h1 className='text-3xl font-semibold'>ToDo List</h1>
-            <Button onClick={handleCreateButtonClick} className='border-none bg-orange-600 rounded-full'>CREATE</Button>
-        </div> */}
         <div className="flex items-center mt-7 justify-between">
           {/* Title Section */}
           <div className="flex items-center gap-2">
@@ -30,14 +29,14 @@ const Todo = () => {
           </div>
           {/* Create Button */}
           <Button
-            onClick={handleCreateButtonClick}
+            onClick={handleAddButtonClick}
             className="border-none bg-orange-600 rounded-full px-4 py-2"
           >
-            CREATE
+            Add +
           </Button>
         </div>
         {/* Create Dialog */}
-        <CreateTaskDialog isOpen={isDialogOpen} onClose={handleCloseDialog} />
+        <CreateTaskDialog isOpen={isDialogOpen} onClose={handleCloseDialog} inputRef={inputRef}/>
         {/* ToDo List */}
     </div> 
   )
